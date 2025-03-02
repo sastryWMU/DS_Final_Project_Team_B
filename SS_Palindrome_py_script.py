0a1e401103a2282eb10538808f1772b92bcdcbc6
Sumasree24 <sumasreems2@gmail.com>
2025-03-01 23:49:20 -0500

    Modified Code Added

--- SS_Palindrome_py_script.py
+++ SS_Palindrome_py_script.py
@@ -7,3 +7,21 @@ if is_palindrome(word):
     print("It's a palindrome!")
 else:
     print("Not a palindrome.")
+
+
+#Code 2: Using for loop
+
+def is_palindrome(s):
+    s = s.lower()  # Convert to lowercase for case insensitivity
+    length = len(s)
+    for i in range(length // 2):
+        if s[i] != s[length - i - 1]:
+            return False
+    return True
+
+# Example usage
+word = input("Enter a word: ")
+if is_palindrome(word):
+    print("It's a palindrome!")
+else:
+    print("Not a palindrome.")
